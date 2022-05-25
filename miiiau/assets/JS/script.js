4b99ab867580a467131b43d3d9a9f416b7dfe591
Mayarabnc <mayarabncabral@gmail.com>
2022-05-25 15:04:55 -0300

    Concertando menu e adicionando Js

--- miiiau/assets/JS/script.js
+++ miiiau/assets/JS/script.js
@@ -1,11 +1,27 @@
+var nome = window.document.querySelector("#nome");
+
+var nomeOk = false
+var emailOk = false
+var assuntoOk = false
+var mapa = document.querySelector("#mapa")
+var email = document.querySelector("#email");
+
+var assunto = document.querySelector("#assunto");
+
+
+email.style.width ="100%";
+assunto.style.width = "100%";
+
 function validarNome(){
-    
+
     let txtNome = document.querySelector("#txtNome");
-   
+
+
     if(nome.value.length < 2){
-                 
+
         txtNome.innerHTML = "nome invalido :(";
 
+  
         txtNome.style.color = "red";
         nomeOk = false;
 
@@ -16,3 +32,75 @@ function validarNome(){
         nomeOk = true;
     }
 }
+
+ function validarEmail(){
+     let txtEmail = document.querySelector("#txtEmail");
+
+
+     if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1){
+
+        txtEmail.innerHTML = "E-mail invalido";
+        txtEmail.style.color = "red";
+        emailOk = false;
+
+     }else{
+
+        txtEmail.innerHTML = "E-mail valido";
+        txtEmail.style.color = "green";
+        emailOk = true;
+
+     }
+
+ }
+
+ function validarAssunto(){
+
+    let txtAssunto = document.querySelector("#txtAssunto");
+
+    if (assunto.value.length >= 100){
+
+        txtAssunto.innerHTML = "Tem uma luzinha que tenho que pegar, digite até 100 caracteres ;)"
+        txtAssunto.style.color = "red"
+        txtAssunto.style.display = "block"
+        assuntoOk = false
+    }else{
+
+ 
+        txtAssunto.style.display = "none"
+ 
+
+        assuntoOk = true
+
+    }
+
+
+ }
+
+
+ function enviar(){
+
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+
+
+      alert("Obrrrrigada pela miausagem");
+
+    }else{
+
+      alert("Você agiu como um cachorro e esse site pra gato");
+
+    }
+ }
+
+ function gatoZoom(){
+
+    mapa.style.width= "800px"
+    mapa.style.height= "600px"
+
+  }
+
+  function gatoNormal(){
+
+    mapa.style.width= "600px"
+    mapa.style.height= "450px"
+
+  }
